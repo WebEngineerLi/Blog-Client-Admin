531e19be0bffec042c2356b6a7fe4e3928c5b515
wb-lmy412939 <wb-lmy412939@alibaba-inc.com>
2019-07-04 15:56:43 +0800

    feat: 添加简单的高阶组件

--- src/App/index.js
+++ src/App/index.js
@@ -3,13 +3,23 @@ import CSSModules from 'react-css-modules';
 import imgjjy from './img/jjy1.jpeg'
 import styles from './index.less';
 
+function HigherComponent(WrappedComponent) {
+	const newProps = {
+		message: '信息',
+	}
+	return props => {
+		return <WrappedComponent {...props} {...newProps} />
+	}
+}
+
+@HigherComponent
 @CSSModules(styles)
 class App extends Component {
 	render() {
 		return (
-      <Fragment>
-        <div styleName="bg-img"></div>
-      </Fragment> 
+			<Fragment>
+				<div styleName="bg-img"></div>
+			</Fragment>
 		)
 	}
 }
