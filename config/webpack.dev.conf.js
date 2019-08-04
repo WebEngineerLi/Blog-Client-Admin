45d6aac84d240e0e875b75ab95e2ef9c1f722707
xiaopanpan <610986797@qq.com>
2019-08-04 17:03:36 +0800

    plugin: 修改webpack配置

--- config/webpack.dev.conf.js
+++ config/webpack.dev.conf.js
@@ -41,7 +41,7 @@ module.exports = merge(base, {
 	},
 	plugins: [
 		new webpack.DefinePlugin({
-			'BASE_URL': JSON.stringify('http://39.97.243.148:7001')
+			'BASE_URL': JSON.stringify('http://localhost:7001')
 		})
 	]
 })
